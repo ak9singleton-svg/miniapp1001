fa6281bb265db4641095eb9df6b459b7225fc0d0
ak9singleton-svg <ak9singleton@gmail.com>
2025-11-11 15:27:01 +0500

    Create rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,49 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+
+const extensions = ['.js', '.jsx'];
+
+export default [
+    // Конфигурация для клиентской части
+    {
+        input: 'src/client.jsx',
+        output: {
+            file: 'public/js/client.js',
+            format: 'iife',
+            name: 'clientBundle',
+            sourcemap: false
+        },
+        plugins: [
+            resolve({ extensions }),
+            commonjs(),
+            babel({
+                extensions,
+                babelHelpers: 'bundled',
+                presets: ['@babel/preset-env', '@babel/preset-react']
+            }),
+            terser({ compress: { drop_console: false } })
+        ]
+    },
+    // Конфигурация для админ-панели
+    {
+        input: 'src/admin.jsx',
+        output: {
+            file: 'public/js/admin.js',
+            format: 'iife',
+            name: 'adminBundle',
+            sourcemap: false
+        },
+        plugins: [
+            resolve({ extensions }),
+            commonjs(),
+            babel({
+                extensions,
+                babelHelpers: 'bundled',
+                presets: ['@babel/preset-env', '@babel/preset-react']
+            }),
+            terser({ compress: { drop_console: false } })
+        ]
+    }
+];
