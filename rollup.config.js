6491554daa1aabb030fdb6c22aecc692e7edafe1
ak9singleton-svg <ak9singleton@gmail.com>
2025-11-11 16:06:13 +0500

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
-// Импортируем НОВЫЙ официальный плагин
-import { terser } from '@rollup/plugin-terser'; 
+// ИСПРАВЛЕНИЕ: Импортируем terser как экспорт по умолчанию (без фигурных скобок)
+import terser from '@rollup/plugin-terser'; 
 
 const extensions = ['.js', '.jsx'];
 
@@ -24,7 +24,7 @@ export default [
                 babelHelpers: 'bundled',
                 presets: ['@babel/preset-env', '@babel/preset-react']
             }),
-            // Используем новый плагин. API вызова остается прежним.
+            // Вызов плагина остается прежним
             terser({ compress: { drop_console: false } }) 
         ]
     },
@@ -45,7 +45,7 @@ export default [
                 babelHelpers: 'bundled',
                 presets: ['@babel/preset-env', '@babel/preset-react']
             }),
-            // Используем новый плагин. API вызова остается прежним.
+            // Вызов плагина остается прежним
             terser({ compress: { drop_console: false } }) 
         ]
     }
